122042bbb4eddace8afc4db1d23b284c3c594dc3
metehanozcan <70481060+metehanozcan@users.noreply.github.com>
2022-08-13 06:37:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,48 @@
 from flask import Flask, render_template,jsonify,request
 from backend import job
-
-
-
+import cchardet
+import time
+import datetime
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-#* Flask uses routes to navigate pages.
-#* You have to use templates for flask.
-
-  
 
 
+old_t = datetime.datetime.utcnow()
+x=[]
+x.append(old_t) #*Server Start Time
+x.append(True) #* IsIniate
+x.append('') #* Response
 @app.route('/')
-@app.route('/index')
-def index():
+async def index():
     
-    return render_template('index.html')
-    
-    # return render_template('index.html',table=[0],table1=[1],table2=[2],table3=[3],table4=[4])
-@app.route('/update', methods=['GET'])
-def update():   
+    dt_started = datetime.datetime.utcnow()
+    if x[1] == True:
+        data= await job()
+        x[1]=False
+        x[2] = data
+        return render_template('index.html',table=data[0],table1=data[1],table2=data[2],table3=data[4],table4=data[3])
+        
 
-    data=job() 
-    # return jsonify(data[0],data[1],data[2],data[3],data[4])
-    return jsonify(table=data[0],table1=data[1],table2=data[2],table3=data[3],table4=data[4])
+        
+    elif (dt_started-x[0]).total_seconds() > 300:         
+        data = await job()
+        x[0]  = datetime.datetime.utcnow()
+        x[2] = data
+        return render_template('index.html',table=data[0],table1=data[1],table2=data[2],table3=data[4],table4=data[3])   
+    
+    elif x[1] == False or (dt_started-x[0]).total_seconds()<300:
+        data_ = x[2]
+        return render_template('index.html',table=data_[0],table1=data_[1],table2=data_[2],table3=data_[4],table4=data_[3])
+    else:
+        return '404 bro. Yamulduk'
+      
+# def update():   
+
+#     data=job() 
+#     # return jsonify(data[0],data[1],data[2],data[3],data[4])
+#     return jsonify(table=data[0],table1=data[1],table2=data[2],table3=data[3],table4=data[4])
 
 if __name__=='__main__':
     app.run()
