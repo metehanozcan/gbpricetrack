c068789e8bfae45a9a3563e58442742a757e7e18
metehanozcan <70481060+metehanozcan@users.noreply.github.com>
2022-08-13 07:45:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,34 +15,41 @@ x.append(old_t) #*Server Start Time
 x.append(True) #* IsIniate
 x.append('') #* Response
 @app.route('/')
-async def index():
+async def index():    
     
-    dt_started = datetime.datetime.utcnow()
-    if x[1] == True:
+    if x[1] == True: #* Iniate 
         data= await job()
         x[1]=False
         x[2] = data
-        return render_template('index.html',table=data[0],table1=data[1],table2=data[2],table3=data[4],table4=data[3])
-        
+        data_ = x[2]
+        return render_template('index.html',table=data_[0],table1=data_[1],table2=data_[2],table3=data_[4],table4=data_[3])
+                
+    elif x[1] == False: #* Update
+        data_ = x[2]        
+        return render_template('index.html',table=data_[0],table1=data_[1],table2=data_[2],table3=data_[4],table4=data_[3])
+    else:
+        return 'Eğer bu yazıyı görüyorsan site gitmiş, geçmiş olsun.'
+   
 
-        
-    elif (dt_started-x[0]).total_seconds() > 300:         
+
+
+    
+
+@app.route('/update', methods=['GET'])
+async def update():
+    dt_started = datetime.datetime.utcnow()   
+    remaining_sec=(dt_started-x[0]).total_seconds()
+    if remaining_sec > 300:         
         data = await job()
         x[0]  = datetime.datetime.utcnow()
         x[2] = data
-        return render_template('index.html',table=data[0],table1=data[1],table2=data[2],table3=data[4],table4=data[3])   
-    
-    elif x[1] == False or (dt_started-x[0]).total_seconds()<300:
-        data_ = x[2]
-        return render_template('index.html',table=data_[0],table1=data_[1],table2=data_[2],table3=data_[4],table4=data_[3])
+        data_ = x[2]   
+        return jsonify(table=data_[0],table1=data_[1],table2=data_[2],table3=data_[3],table4=data_[4])
     else:
-        return '404 bro. Yamulduk'
-      
-# def update():   
+        return F'{remaining_sec} ,seconds passed.'
+    
 
-#     data=job() 
-#     # return jsonify(data[0],data[1],data[2],data[3],data[4])
-#     return jsonify(table=data[0],table1=data[1],table2=data[2],table3=data[3],table4=data[4])
-
-if __name__=='__main__':
+    
+                
+if __name__=='__main__':  
     app.run()
